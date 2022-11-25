5e6a16f2a202d82410540ccd66aed5d31d7b649b
Denis Danilov <denis.danilov.2801200152@gmail.com>
2022-11-26 01:28:09 +0300

    Создал компоненты

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Header from "./components/Header"
+import Card from "./components/Card"
+import Drawer from "./components/Drawer"
+
+function App() {
+  return (
+    <div className="wrapper clear">
+      <Drawer />
+      <Header />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиcк..." />
+          </div>
+        </div>
+
+        <div className="d-flex">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+function Header() {
+  return (
+    <header className="header d-flex justify-between align-center p-40">
+      <div className="d-flex align-center">
+        <img width={40} height={40} alt="Logo" src="/img/logo.png" />
+        <div>
+          <h3 className="text-uppercase">React sneakers</h3>
+          <p className="opacity-5">Магазин лучших кроссовок</p>
+        </div>
+      </div>
+      <ul className="header Right d-flex">
+        <li className="mr-30">
+          <img width={18} height={18} alt="Cart" src="/img/cart.svg" />
+          <span>1205 руб.</span>
+        </li>
+        <li>
+          <img width={18} height={18} alt="Heart" src="/img/heart.svg" />
+          <img width={18} height={18} alt="User" src="/img/user.svg" />{" "}
+        </li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
